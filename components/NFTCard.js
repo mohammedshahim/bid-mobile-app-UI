f50a9c9df5712fe05d49d82a23852d3e6dc0578c
Shahim <muhdshahim786@gmail.com>
2022-06-23 15:27:57 +0400

    home page design completed

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -1,7 +1,7 @@
 import { View, Image, Text } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { COLORS, SIZES, SHADOWS, assets } from "../constants";
-import { CircleButton } from "./Button";
+import { CircleButton, RectButton } from "./Button";
 import { SubInfo, EthPrice, NFTTitle } from "./SubInfo";
 
 const NFTCard = ({ data }) => {
@@ -33,6 +33,30 @@ const NFTCard = ({ data }) => {
       </View>
 
       <SubInfo />
+
+      <View style={{ width: "100%", padding: SIZES.font }}>
+        <NFTTitle
+          title={data.name}
+          subTitle={data.creator}
+          titleSize={SIZES.large}
+          subTitleSize={SIZES.small}
+        />
+        <View
+          style={{
+            marginTop: SIZES.font,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <EthPrice price={data.price} />
+          <RectButton
+            minWidth={120}
+            fontSize={SIZES.font}
+            handlePress={() => navigation.navigate("Details", { data })}
+          />
+        </View>
+      </View>
     </View>
   );
 };
